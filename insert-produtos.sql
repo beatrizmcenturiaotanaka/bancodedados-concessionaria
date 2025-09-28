954cec3ebbc39c13856d6181eff9bc6f87e79436
Beatriz M Centuriao Tanaka <b.centuriao@gmail.com>
2025-09-27 21:46:54 -0300

    Inserindo produtos

--- insert-produtos.sql
+++ insert-produtos.sql
@@ -4,7 +4,7 @@ INSERT INTO produtos (nome, descricao, preco, quantidade) VALUES
 ('Carro B', 'SUV espaçoso com excelente desempenho off-road.', 80000.00, 5),
 ('Carro C', 'Sedã de luxo com tecnologia avançada e conforto superior.', 120000.00, 3),
 ('Carro D', 'Hatchback esportivo com design moderno e alta performance.', 60000.00, 7),
-('Carro E', 'Caminhonete robusta para trabalho pesado e aventuras ao ar livre.', 90000.00, 4);
+('Carro E', 'Caminhonete robusta para trabalho pesado e aventuras ao ar livre.', 90000.00, 5);
 
 -- buscando produtos
 SELECT * FROM produtos;
